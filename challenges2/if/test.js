18eb876de9997480dd9022292aaa4e08a8a250d8
daffamf <daffamuhammadfirdaus5@gmail.com>
2025-10-15 20:45:53 +0700

    challenges 2

---
+++ challenges2/if/test.js
@@ -0,0 +1,11 @@
+let umur = 20;
+
+if (umur >= 18) {
+  console.log("Anda sudah dewasa");
+}
+
+if (umur >= 18) {
+  console.log("Anda bisa memilih dalam pemilu");
+}
+
+console.log("Terima kasih");
